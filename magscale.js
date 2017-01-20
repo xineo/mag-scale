2a206329f285702b1a71df987e7497fddeb3a6c4
xineo <schardon@freeice.org>
2017-01-20 17:27:14 +0100

    Update magscale.js

--- magscale.js
+++ magscale.js
@@ -1,27 +1,9 @@
-// This is your first fully working Tabris.js app. Feel free to alter as you please.
-// Changes are saved automatically and are immediately available on your device.
-
-// Create a top-level page that contains our UI
 var page = new tabris.Page({
-  title: "Hello, World!",
-  topLevel: true
+  topLevel: true,
+  title: "myapp"
 });
-
-// Create a push button and add it to the page
-var button = new tabris.Button({
-  centerX: 0, top: 100,
-  text: "Native Widgets"
+new tabris.TextView({
+  layoutData: {centerX: 0, centerY: 0},
+  text: "My First App"
 }).appendTo(page);
-
-// Create a text view and add it too
-var textView = new tabris.TextView({
-  centerX: 0, top: [button, 50],
-  font: "24px"
-}).appendTo(page);
-
-// Change the text view's text when the button is pressed
-button.on("select", function() {
-  textView.set("text", "Totally Rock!");
-});
-
 page.open();
